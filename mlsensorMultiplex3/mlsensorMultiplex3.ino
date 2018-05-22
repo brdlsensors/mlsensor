2b5852be453cbbfed83ad38b7036332e4b07cc1a
Ben Shih <benjshih@gmail.com>
2018-05-21 18:32:44 -0700

    updated multiplexer code to be 100 Hz

--- mlsensorMultiplex3/mlsensorMultiplex3.ino
+++ mlsensorMultiplex3/mlsensorMultiplex3.ino
@@ -27,7 +27,7 @@ void setup() {
   TCCR1B = 0;
   TCNT1  = 0;
 
-  OCR1A = 625/5;            // compare match register 16MHz/256/2Hz
+  OCR1A = 625;            // compare match register 16MHz/256/2Hz
   TCCR1B |= (1 << WGM12);   // CTC mode
   TCCR1B |= (1 << CS12);    // 256 prescaler 
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
