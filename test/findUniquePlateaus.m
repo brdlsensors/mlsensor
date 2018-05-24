04310ada958d00cd06e9dd26f4118c916bc50641
Ben Shih <benjshih@gmail.com>
2018-05-23 18:16:56 -0700

    hacked together version of extracting the multiplex6 data.

--- test/findUniquePlateaus.m
+++ test/findUniquePlateaus.m
@@ -2,32 +2,57 @@ clc
 clear all
 close all
 
-load('t1.mat');
-i = 64;
-sig(i,:) = abs(pwc_cluster(singleReading(i,:),[],0,0.05,1));
+numSensors = 3;
+load('t1_3contacts_2.mat');
 
-% How to identify array elements that occur more than once
-sig_filt = sig(i,:);
-idx = find(hist(sig_filt,unique(sig_filt))>4); % current challenge is that histogram sorts manually
-uniqVals = unique(sig_filt);
-plateau = uniqVals(idx)
+%%
 
-[counts, centers] = hist(sig_filt, unique(sig_filt))
+[m,n] = size(singleReading);
 
-figure;
-plot(sig_filt)
+plateau_sigs = zeros(length(singleReading), numSensors);
 
-ordering = zeros(size(plateau));
-for i=1:length(plateau)
-    vals = find(sig_filt == plateau(i));
-    ordering(i) = vals(1);
+for i = 1:length(singleReading)
+    sig(i,:) = abs(pwc_cluster(singleReading(i,:),[],0,0.1,1));
+    [v, ind] = max(sig(i,:));
+    sig(i,:) = circshift(sig(i,:), n+1-ind);
+
+    % How to identify array elements that occur more than once
+    sig_filt = sig(i,:);
+    idx = find(hist(sig_filt,unique(sig_filt))>4); % current challenge is that histogram sorts manually
+    uniqVals = unique(sig_filt);
+    plateau = uniqVals(idx);
+
+    [counts, centers] = hist(sig_filt, unique(sig_filt));
+
+    %figure;
+    %plot(sig_filt)
+
+    ordering = zeros(size(plateau));
+    for k=1:length(plateau)
+        vals = find(sig_filt == plateau(k));
+        ordering(k) = vals(1); % take the first value that represents the plateau.
+    end
+
+    [ordering_sorted, ordering_order] = sort(ordering);
+    plateau_sorted = plateau(ordering_order);
+    
+    if(length(plateau_sorted) < 4)
+       plateau_sorted = [plateau_sorted, plateau_sigs(i-1, end)]; 
+    end
+    if(length(plateau_sorted) < 5)
+        plateau_sorted = [plateau_sorted, plateau_sorted(4)];
+    end
+    
+    plateau_sigs(i,:) = [plateau_sorted(2), plateau_sorted(4), plateau_sorted(5)];
 end
 
-%%
-[ordering_sorted, ordering_order] = sort(ordering)
-plateau_sorted = plateau(ordering_order)
+
+%plot(plateau_sigs,'DisplayName','plateau_sigs')
 
 
-
-%%
-
+windowSize = 5;
+plateau_sigFilt = zeros(size(plateau_sigs));
+for i=1:numSensors
+    plateau_sigFilt(:,i) = medfilt1(plateau_sigs(:,i), windowSize);
+end
+plot(plateau_sigFilt, 'DisplayName', 'plateau_sigFilt');
