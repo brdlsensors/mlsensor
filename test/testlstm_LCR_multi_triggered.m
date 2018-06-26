c05314a6f3b1df1027e4c52e6eec30787fa512b2
Ben Shih <benjshih@gmail.com>
2018-06-26 15:10:52 -0700

    fixed comments for testlstm_LCR_multi_triggered.m

--- test/testlstm_LCR_multi_triggered.m
+++ test/testlstm_LCR_multi_triggered.m
@@ -108,24 +108,24 @@ for i = 1:timeStepEnd
     t(i,1) = toc;
 
     %% Online learning.
-    inp=inp2(1,ceil(t(i,1)));
-    outr(i,:)=out(i,:); %-out(1,:)+[4673 -1912 2778 -1358 3046 -1263];
+    inp=inp2(1,ceil(t(i,1))); % interpolate the data if it's not already at 10 Hz.
+    outr(i,:)=out(i,:); %-out(1,:)+[4673 -1912 2778 -1358 3046 -1263]; % take the values of outp at the current instance in time, i, and convert from 3d to 2d.
 
     if i>1
         for k=1:6 % RX channels x3, for 3 sensors.
             % [tempval,yt]=resample(outr(i-1:i,k),t(i-1:i,1),10,'linear');
-            % ???
-            outp(1,k)=outr(i-1,k)+(outr(i,k)-outr(i-1,k))*0.1/(t(i)-t(i-1));
+            % Linear interpolation. to interpolate the data 10 Hz.
+            outp(1,k)=outr(i-1,k)+(outr(i,k)-outr(i-1,k))*0.1/(t(i)-t(i-1)); 
         end
         % inplstm=[inp,out(1:i,rx),outpf(1:i-1,rx)]';
-        inplstm=[inp,outp(1,:)]';
+        inplstm=[inp,outp(1,:)]'; % only need to train on the current instance in time.
         
         % Normalize the input for LSTM.
-        inplstm= inplstm-xm(:);
-        inplstm= inplstm./xs(:);
+        inplstm = inplstm-xm(:);
+        inplstm = inplstm./xs(:);
         
         % NN predict and update state. 
-        [net,YPred_o(:,i) ]= predictAndUpdateState(net,inplstm);
+        [net,YPred_o(:,i)] = predictAndUpdateState(net,inplstm);
         % Undo normalization.
         YPred_o(1,i)=YPred_o(1,i)*ts(1)+tm(1);
         YPred_o(2,i)=YPred_o(2,i)*ts(2)+tm(2);
@@ -136,7 +136,7 @@ for i = 1:timeStepEnd
         plot(YPred_o(1,1:i),'b')
         drawnow()
         hold on
-        plot(squeeze(pos(1,1:i,2))-squeeze(pos(1,1:i,1)),'r')
+        plot(squeeze(pos(1,1:i,2))-squeeze(pos(1,1:i,1)),'r') % *position COULD be off slightly because it's not being interpolated upon.
     end
 
     %a=t(i,1);
