62af87bc6a6b7c00930580be8d7864f28de5e0ed
Ben Shih <benjshih@gmail.com>
2018-06-28 03:45:16 -0700

    finished adding comments to the most commonly used files

--- test/joining.m
+++ test/joining.m
@@ -1,4 +1,8 @@
+% This file is used manually (copy&paste the sections) to merge two sets of
+% vectors (inp, outp, posp) between two workspaces (.mat files) for testing
+% different combinations of data for training.
 
+%% Account for the total size of the new vector and merge the two sets of vectors.
 siz=18000;
 
 %load('t3_4contcts.mat','inp','outp','posp')
@@ -12,11 +16,11 @@ posp=posp(:,1:siz,:);
 a1=[a1; inp];
 b1=[b1; outp];
 c1=cat(2,c1, posp);
-%%
+%% Temp vars for curr vectors.
 a1=[inp];
 b1=[outp];
 c1=posp;
-%%
+%% Restore to original file name.
  
 inp=a1;
 outp=b1;
