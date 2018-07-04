7b911da9baccd07dc62b6cdf4c278af8e0bfd57e
tomraven1 <thomasgeorge2571766@gmail.com>
2018-07-03 18:24:54 -0700

    news plots and rearranging results data again2..sorry

--- test/results_good/commercial_results/plotting_codes.m
+++ test/results_good/commercial_results/plotting_codes.m
@@ -14,20 +14,38 @@ pos_pred=YPred_o(:,1:test_per);
 
 err=rssq(pos_pred-pos_ref);
 
-mean(err(50:end-50))
-plot(err)
-
-plot(forc(1:i))
+subplot(3,1,1)
+plot(pos_pred(1,:))
 hold on
-plot(YPred_o(end,1:i))
+plot(pos_ref(1,:))
 
-
-plot(YPred_o(1,1:i),'b')
-%                 drawnow()
+subplot(3,1,2)
+plot(pos_pred(2,:))
 hold on
-plot(squeeze(pos(1,1:i,2))-squeeze(pos(1,1:i,1)),'r') % *position COULD be off slightly because it's not being interpolated upon.
+plot(pos_ref(2,:))
 
+subplot(3,1,3)
+plot(pos_pred(3,:))
+hold on
+plot(pos_ref(3,:))
 
-[a,ind]=max(pos_ref(3,:));
-[a,ind2]=min(pos_ref(3,:));
-rssq(pos_ref(:,ind)-pos_ref(:,ind2))
+% subplot(4,1,4)
+% plot(err)
+
+% mean(err(50:end-50))
+% plot(err)
+
+% plot(forc(1:i))
+% hold on
+% plot(YPred_o(end,1:i))
+% 
+% 
+% plot(YPred_o(1,1:i),'b')
+% %                 drawnow()
+% hold on
+% plot(squeeze(pos(1,1:i,2))-squeeze(pos(1,1:i,1)),'r') % *position COULD be off slightly because it's not being interpolated upon.
+% 
+% 
+% [a,ind]=max(pos_ref(3,:));
+% [a,ind2]=min(pos_ref(3,:));
+% rssq(pos_ref(:,ind)-pos_ref(:,ind2))
